36b687e55e10153a40435dc672e8baae2607e2b6
Chris Farmer <WS311471@weston.ac.uk>
2021-05-11 14:35:36 +0100

    Completed excersises

--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -11,8 +11,6 @@ struct Person {
     age: usize,
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -26,6 +24,26 @@ struct Person {
 impl FromStr for Person {
     type Err = Box<dyn error::Error>;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err("Empty string found".into());
+        }
+
+        let args: Vec<&str> = s.split(',').collect();
+        if args.len() != 2 {
+            return Err("To many arguments found".into())
+        }
+
+        if args[0].len() == 0 {
+            return Err("Name is an empty string".into());
+        }
+
+        match args[1].to_string().parse::<usize>() {
+            Ok(age) => Ok(Person {
+                name: args[0].to_string(),
+                age
+            }),
+            Err(_e) => Err("Could not parse age".into())
+        }
     }
 }
 
